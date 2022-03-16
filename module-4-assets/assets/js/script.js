f7238776a8adda4de0de97bcd51af9fa398a631f
Sultan Siddiqi <sultan.siddiqi@gmail.com>
2022-03-15 23:13:32 -0400

    finished 4.2.6

--- module-4-assets/assets/js/script.js
+++ module-4-assets/assets/js/script.js
@@ -1,21 +1,25 @@
-console.dir(window.document);
-
 var formEl = document.querySelector("#task-form");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-    
-    event.preventDefault();
-    
-    console.log(event);
+var createTaskHandler = function (event) {
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var listeItemEl = document.createElement("li");
-    listeItemEl.className = "task-item";
-    listeItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listeItemEl);
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
